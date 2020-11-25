82c31e6ae65b1fcd675313be52b2ca28745530bc
Yage Hu <yagejhu@gmail.com>
2020-11-24 20:43:59 -0800

    Update version to 0.14.4

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ pub use document::Document;
 pub use export::{Export, InlineExport};
 pub use expression::{
     AsAtoms, Block, Br, BrIf, BrTable, Call, Drop, Expression, GlobalGet,
-    GlobalSet, I32Add, I32Const, I32Eq, I32Eqz, I32GtU, I32Ne, I32Sub,
+    GlobalSet, I32Add, I32Const, I32Eq, I32Eqz, I32GtU, I32Ne, I32RemU, I32Sub,
     I64Const, If, Instruction, Local, LocalGet, LocalSet, LocalTee, Loop, Then,
 };
 pub use import_desc::{ImportDesc, ImportDescFunc};
